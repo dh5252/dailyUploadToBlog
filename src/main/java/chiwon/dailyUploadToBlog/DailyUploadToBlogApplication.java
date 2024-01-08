2bd31ebb64a4bb2be6d58b36e7c624b1746f410b
chiwon <dh5277@naver.com>
2024-01-08 19:58:10 +0900

    first

---
+++ src/main/java/chiwon/dailyUploadToBlog/DailyUploadToBlogApplication.java
@@ -0,0 +1,13 @@
+package chiwon.dailyUploadToBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyUploadToBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyUploadToBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chiwon/dailyUploadToBlog/DailyUploadToBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package chiwon.dailyUploadToBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyUploadToBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
